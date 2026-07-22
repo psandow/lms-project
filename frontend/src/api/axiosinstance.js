13544d7e92e26e53f7268dcb82c59265611c3547
Paul Sandow <psandow@gmail.com>
2026-07-22 11:14:08 +0100

    Add Response interceptor to axiosinstance for auto refresh of JWT access tokens

--- frontend/src/api/axiosinstance.js
+++ frontend/src/api/axiosinstance.js
@@ -11,6 +11,43 @@ axiosInstance.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
-});
+},
+(error) => Promise.reject(error)
+);
+
+// Repsonse interceptor detects expired access tokens
+axiosInstance.interceptors.response.use(
+  (response) => response,
+
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      const refresh = localStorage.getItem("refresh");
+      if (!refresh) {
+        return Promise.reject(error);
+      }
+
+      try {
+        // Ask backend for a new access token
+        const res = await axios.post("/api/auth/refresh/", { refresh });
+
+        const newToken = res.data.access;
+        localStorage.setItem("access", newToken);
+
+        // Retry original request with new access token
+        originalRequest.headers.Authorization = `Bearer ${newToken}`;
+        return axiosInstance(originalRequest);
+
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
