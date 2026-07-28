2d18627568a818876ff3f9d8154fc482d14ea243
Paul Sandow <psandow@gmail.com>
2026-07-28 17:07:49 +0100

    allowed hosts and debug updated in settings.py

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -31,9 +31,13 @@ if env_path.exists():
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.environ.get("RENDER_EXTERNAL_HOSTNAME"),
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
